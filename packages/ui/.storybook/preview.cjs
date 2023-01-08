8f02c3413d44f52c5cfa23b2894e439c93895356
carlos <carl.devfs@gmail.com>
2023-01-08 14:33:53 +0100

    update storybook config

--- packages/ui/.storybook/preview.cjs
+++ packages/ui/.storybook/preview.cjs
@@ -1,9 +1,16 @@
+const FA = document.createElement('link')
+FA.rel = 'stylesheet'
+FA.href = 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+document.head.appendChild(FA)
+import "../src/styles/globalStyles.scss";
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: {argTypesRegex: "^on[A-Z].*"},
+    viewMode: 'docs',
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 }
