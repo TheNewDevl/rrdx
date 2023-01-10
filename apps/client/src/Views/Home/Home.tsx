2a06d4cf7ef942eaae9b719785fac1cdf4a7908f
carlos <carl.devfs@gmail.com>
2023-01-10 21:33:35 +0100

    client: populate profile page

--- apps/client/src/Views/Home/Home.tsx
+++ apps/client/src/Views/Home/Home.tsx
@@ -1,52 +1,16 @@
-import style from "./Home.module.scss";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { authLogout, fetchOrUpdateAuth } from "../../store/features/auth";
-import { fetchOrUpdateUser, selectUser, userLogout } from "../../store/features/user";
-import { useEffect } from "react";
-import * as domain from "domain";
 import { FeatureCard, FlexContainer, Title, Hero } from "@rrdx-mono/ui";
 import { features, hero } from "../../mockup/homeMockup";
 
-interface HomeProps {}
-
-export const Home = ({}: HomeProps) => {
-  /*  const user = useAppSelector(selectUser);
-    const credentials = {
-      email: "tony@stark.com",
-      password: "password123",
-    };
-    const upCredentials = {
-      firstName: "ony",
-      lastName: "tark",
-    };
-    const dispatch = useAppDispatch();
-    useEffect(() => {
-      console.log(user);
-    }, [user]);*/
-
+export const Home = () => {
   return (
     <main style={{ flex: 1 }}>
       <Hero subTittles={hero.subtitles} titleText={hero.title} paragraphs={hero.text} backgroundImg={hero.heroBg} />
       <FlexContainer element={"section"}>
         <Title srOnly children={"Features"} />
         {features.map((ft, i) => (
-          <FeatureCard imgSrc={ft.imgSrc} imgAlt={ft.imgAlt} titleText={ft.tittleText} desc={ft.desc} />
+          <FeatureCard key={i} imgSrc={ft.imgSrc} imgAlt={ft.imgAlt} titleText={ft.tittleText} desc={ft.desc} />
         ))}
       </FlexContainer>
-      {/* <div className={style.Home}>
-        Home view
-        <button onClick={() => dispatch(fetchOrUpdateAuth(credentials))}>Login</button>
-        <button onClick={() => dispatch(fetchOrUpdateUser())}>get user</button>
-        <button onClick={() => dispatch(fetchOrUpdateUser(upCredentials))}>update user</button>
-        <button
-          onClick={() => {
-            dispatch(authLogout());
-            dispatch(userLogout());
-          }}
-        >
-          logout
-        </button>
-      </div>*/}
     </main>
   );
 };
--- apps/client/src/Views/Profile/Profile.tsx
+++ apps/client/src/Views/Profile/Profile.tsx
@@ -1,9 +1,45 @@
-import style from "./Profile.module.scss";
+import { AccountsCard, Button, Input, Title } from "@rrdx-mono/ui";
+import { account } from "../../mockup/account";
+import { useState } from "react";
 
 interface ProfileProps {}
 
 export const Profile = ({}: ProfileProps) => {
-  return <div className={style.Profile}>profile view</div>;
+  const [isEditing, setIsEditing] = useState(false);
+  const handleNameValues = () => {};
+  const handleSubmit = () => {};
+
+  return (
+    <main style={{ flex: 1, backgroundColor: "#12002b" }}>
+      {}
+      <Title level={"1"}>
+        Welcome back
+        <br />
+        {!isEditing && "Tony Jarvis!"}
+      </Title>
+      {isEditing ? (
+        <div>
+          <Input label={"First name"} type={"text"} onChange={handleNameValues} />
+          <Input label={"Last name"} type={"text"} onChange={handleNameValues} />
+          <Button text={"Save"} onClick={handleSubmit} />
+          <Button text={"Cancel"} onClick={() => setIsEditing(false)} />
+        </div>
+      ) : (
+        <Button text={"Edit Name"} onClick={() => setIsEditing(true)} />
+      )}
+      <Title level={"2"} srOnly children={"Accounts"} />
+      {account.map((ac, i) => (
+        <AccountsCard
+          key={i}
+          account={ac}
+          CTA={{
+            CTAText: "View transactions",
+            CTAAction: () => {},
+          }}
+        />
+      ))}
+    </main>
+  );
 };
 
 /** Created by carlos on 08/01/2023 */
