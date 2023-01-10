cbcdf488972bb6f66d69022e4a2c113e2ea90d9e
carlos <carl.devfs@gmail.com>
2023-01-10 23:17:11 +0100

    client: implement the login feature

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,8 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./Router/AppRouter";
 import { Footer, Header } from "@rrdx-mono/ui";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { authSetToken, selectAuth } from "./store/features/auth";
+import { useEffect } from "react";
+import { fetchOrUpdateUser } from "./store/features/user";
 
 function App() {
+  const remember = useAppSelector(selectAuth).remember;
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    //if user checked "remember me" checkbox and a token is present in local storage
+    //set the token in the store and fetch user data
+    if (remember && localStorage.getItem("token")) {
+      dispatch(authSetToken(JSON.parse(localStorage.getItem("token")!)));
+      dispatch(fetchOrUpdateUser());
+    }
+  }, [remember]);
   return (
     <BrowserRouter>
       <Header />
